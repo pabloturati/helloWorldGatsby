98e88e7790f70ade2db6478968547d7135fd92f5
pabloturati <pablo.turati@gmail.com>
2018-08-27 20:57:27 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Pablo Turati's very own Hello WORLD!!!!</h1>
+    <p>If you got this far... you can go farther.</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+)
+
+export default IndexPage
